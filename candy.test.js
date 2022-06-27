ff74c00079f5800a8eeb61cd298fb3a01d90fe9d
Amir Khan <ak2022dev@gmail.com>
2022-06-27 16:15:46 +0100

    Test drove to pass second test

--- candy.test.js
+++ candy.test.js
@@ -7,4 +7,8 @@ describe('Candy', () => {
     const candy = new Candy('Mars', 4.99);
     expect(candy.getName()).toBe('Mars');
   });
+  it('remembers price', () => {
+    const candy = new Candy('Mars', 4.99);
+    expect(candy.getPrice()).toBe(4.99);
+  });
 });
