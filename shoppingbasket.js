cff327f748a8673c4a7a2e9388d428cae92e95f1
Amir Khan <ak2022dev@gmail.com>
2022-06-27 17:57:15 +0100

    Test drove with double to second unit-test in second suite

--- shoppingbasket.js
+++ shoppingbasket.js
@@ -1,14 +1,26 @@
 // File: shoppingbasket.js
 
 class ShoppingBasket {
+
   constructor() {
     this.basket = []
   }
+
+  addItem( item ) {
+    this.basket.push( item );
+  }
+
   getTotalPrice() {
     if (this.basket.length === 0) {
       return 0;
     }
+    else {
+      let total = 0;
+      this.basket.forEach( (item) => { total += item.getPrice(); } );
+      return total;
+    }
   }
+
 }
 
 module.exports = ShoppingBasket;
--- shoppingbasket.test.js
+++ shoppingbasket.test.js
@@ -1,10 +1,16 @@
 // File: shoppingbasket.test.js
 
 const ShoppingBasket = require('./shoppingbasket');
+const candyDouble = { getPrice: () => 3.99 }
 
 describe('ShoppingBasket', () => {
   it('Has a zero total price when first created', () => {
     let basket = new ShoppingBasket();
     expect(basket.getTotalPrice()).toBe(0);
+  });
+  it('Returns the price of a single object when only one in basket', () => {
+    let basket = new ShoppingBasket();
+    basket.addItem( candyDouble );
+    expect(basket.getTotalPrice()).toBe(candyDouble.getPrice());
   });
 });
